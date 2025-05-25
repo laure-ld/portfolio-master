9d0603feedc594241059ad6769668cc616530420
Laure <laure_ld@outlook.fr>
2025-05-25 21:01:19 +0200

    feat(skills): maj des technologies en cours d’etud

--- portfolio/src/components/skills.jsx
+++ portfolio/src/components/skills.jsx
@@ -1,11 +1,11 @@
-function skills () {
+function Skills () {
     return(
         <div className="skill-container">
             <div className="skill" id="competence">
                 <h2>Mes compétences</h2>
                 <button onClick={() => window.open("https://miro.com/welcomeonboard/NHA4TFZvMnpUUHVZVDZmQlVBaUxQTW4vWGRKeGk3WUF3SkJkQU1IZjhPeDlLNTYvZzdEL09ja0FjeVowakVqUnpIVE9BYXR4VXd1Vkg5NXB2b3dqbHVnS0hST1l1TU0zdjlqNkhZUnFmOWF6emtZc1ZXd0VHMjlQK1VVSFQyamZnbHpza3F6REdEcmNpNEFOMmJXWXBBPT0hdjE=?share_link_id=88976010551", "_blank")} className="cards-skills">Retrouvez ma carte mentale</button>
                 <div className="schema-skills">
-                    <img src="/images/graph.png" alt="Mes compétences technique"/>
+                    <img src="/images/graph.png" alt="Carte graphique des compétences techniques"/>
                 </div>
             </div>      
             <div className="softSkill" id="soft">
@@ -32,12 +32,12 @@ function skills () {
                     <h2>Les techniques vers lesquelles je me dirige</h2>
                     <ul>
                         <li className="skill-circle">
-                        <p>Python</p>
-                            <img src="/images/python.png" alt="Python" className="learn-python"/>
+                        <p>Typescript</p>
+                            <img src="/images/TS.svg" alt="typescript" className="learn-ts"/>
                         </li>
                         <li className="skill-circle">
-                            <p>Web3</p>
-                            <img src="/images/web3.png" alt="WEB3" className="learn-web3"/>
+                            <p>Docker</p>
+                            <img src="/images/docker.svg" alt="Docker" className="learn-docker"/>
                         </li>
                         <li className="skill-circle">
                         <p>FastAPI</p>
@@ -50,4 +50,4 @@ function skills () {
     )
 }
 
-export default skills
+export default Skills

eed8b639cb903b19bd8ba43c365b79c943984f58
Laure <laure_ld@outlook.fr>
2025-05-25 10:06:53 +0200

    maj du carrasouls design/ux

--- portfolio/src/components/carrousel.jsx
+++ portfolio/src/components/carrousel.jsx
@@ -3,6 +3,22 @@ import Slider from "react-slick";
 import data from "../data/projects.json";
 import "../styles/carrousels.scss";
 
+function PrevArrow({ className, onClick }) {
+  return (
+    <div className={`${className} custom-arrow`} onClick={onClick}>
+      &#8592; {/* ← flèche gauche */}
+    </div>
+  );
+}
+
+function NextArrow({ className, onClick }) {
+  return (
+    <div className={`${className} custom-arrow`} onClick={onClick}>
+      &#8594; {/* → flèche droite */}
+    </div>
+  );
+}
+
 function Carrousels() {
   const settings = {
     className: "slider variable-width",
@@ -10,18 +26,20 @@ function Carrousels() {
     centerMode: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    variableWidth: true
+    variableWidth: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
   };
 
   return (
     <div className="slider-container">
       <Slider {...settings}>
-      {data.map((project, index) => (
-        <div key={index} className="slide">
-          <img src={project.image} alt={`${project.name} image`} />
-          <h3>{project.name}</h3>
-        </div>
-      ))}
+        {data.map((project, index) => (
+          <div key={index} className="slide">
+            <img src={project.image} alt={`${project.name} image`} />
+            <h3>{project.name}</h3>
+          </div>
+        ))}
       </Slider>
     </div>
   );
